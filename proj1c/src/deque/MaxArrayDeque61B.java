473b229b50edf0294b249da073f44f07b57e562f
yyang <yyang.phdpe@gmail.com>
2024-10-08 09:49:56 -0400

    MaxArrayDeque completed

--- proj1c/src/deque/MaxArrayDeque61B.java
+++ proj1c/src/deque/MaxArrayDeque61B.java
@@ -2,7 +2,7 @@ package deque;
 import java.util.Comparator;
 import java.util.Collections;
 
-public class MaxArrayDeque61B<T> extends ArrayDeque61B<T>{
+public class MaxArrayDeque61B<T> extends ArrayDeque61B<T> {
     private Comparator<T> newComparator;
 
     public MaxArrayDeque61B(Comparator<T> c) {
@@ -11,35 +11,59 @@ public class MaxArrayDeque61B<T> extends ArrayDeque61B<T>{
     }
 
     public T max() {
-        T returnmax = null;
+        T returnmax;
         if (size == 0) {
             return null;
         } else {
+            returnmax = items[nextFirst + 1];
             if (nextFirst + 1 < nextLast - 1) {
-                for (int i = nextFirst + 1; i < nextLast - 1; i++) {
-                    if (newComparator.compare(items[i], items[i+1]) <= 0) {
+                for (int i = nextFirst + 1; i < nextLast; i++) {
+                    if (newComparator.compare(returnmax, items[i]) <= 0) {
                         returnmax = items[i];
-                    };
+                    }
                 }
             } else if (nextFirst + 1 > nextLast - 1) {
-                for (int i = nextFirst + 1; i < items.length - 1; i++) {
-                    if (newComparator.compare(items[i], items[i+1]) <= 0) {
+                for (int i = nextFirst + 1; i < items.length; i++) {
+                    if (newComparator.compare(returnmax, items[i]) <= 0) {
                         returnmax = items[i];
-                    };
+                    }
                 }
                 for (int i = 0; i < nextLast; i++) {
                     if (newComparator.compare(returnmax, items[i]) <= 0) {
                         returnmax = items[i];
-                    };
+                    }
                 }
             }
-
         }
        return returnmax;
     }
 
     public T max(Comparator<T> c) {
-        return null;
+        T returnmax;
+        if (size == 0) {
+            return null;
+        } else {
+            returnmax = items[nextFirst + 1];
+            if (nextFirst + 1 < nextLast - 1) {
+                for (int i = nextFirst + 1; i < nextLast; i++) {
+                    if (c.compare(returnmax, items[i]) <= 0) {
+                        returnmax = items[i];
+                    }
+                }
+            } else if (nextFirst + 1 > nextLast - 1) {
+                for (int i = nextFirst + 1; i < items.length; i++) {
+                    if (c.compare(returnmax, items[i+1]) <= 0) {
+                        returnmax = items[i];
+                    }
+                }
+                for (int i = 0; i < nextLast; i++) {
+                    if (c.compare(returnmax, items[i]) <= 0) {
+                        returnmax = items[i];
+                    }
+                }
+            }
+        }
+        return returnmax;
     }
 
 }
--- proj1c/tests/MaxArrayDeque61BTest.java
+++ proj1c/tests/MaxArrayDeque61BTest.java
@@ -20,5 +20,16 @@ public class MaxArrayDeque61BTest {
         mad.addFirst("2");
         mad.addFirst("fury road");
         assertThat(mad.max()).isEqualTo("fury road");
+        assertThat(mad.max(new StringLengthComparator())).isEqualTo("fury road");
+    }
+
+    @Test void naturalOrderTest() {
+        MaxArrayDeque61B<Integer> m = new MaxArrayDeque61B<Integer>(Comparator.naturalOrder());
+        m.addFirst(9);
+        m.addFirst(10);
+        m.addFirst(8);
+        m.addFirst(2);
+        assertThat(m.max()).isEqualTo(10);
+        assertThat(m.max(Comparator.naturalOrder())).isEqualTo(10);
     }
 }
