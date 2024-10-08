374481582515fa2e90076cab5abd246c0f227ec5
yyang <yyang.phdpe@gmail.com>
2024-10-08 10:27:54 -0400

    GuitarString started

--- proj1c/src/gh2/GuitarString.java
+++ proj1c/src/gh2/GuitarString.java
@@ -2,6 +2,9 @@ package gh2;
 
 // TODO: maybe more imports
 
+import deque.ArrayDeque61B;
+import deque.Deque61B;
+
 //Note: This file will not compile until you complete the Deque61B implementations
 public class GuitarString {
     /** Constants. Do not change. In case you're curious, the keyword final
@@ -12,7 +15,7 @@ public class GuitarString {
 
     /* Buffer for storing sound data. */
     // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque61B<Double> buffer;
+    private Deque61B<Double> buffer = new ArrayDeque61B<>();
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
@@ -20,8 +23,11 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer with zeros.
+        int capacity = (int) Math.round(SR / frequency);
+        for (int i = 0; i < capacity; i++) {
+            buffer.addFirst(0.0);
+        }
     }
-
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
@@ -33,6 +39,9 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+        for (double r : buffer) {
+            r = Math.random() - 0.5;
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
